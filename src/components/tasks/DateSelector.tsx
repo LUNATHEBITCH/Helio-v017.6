76fea3bbae7c38ee3e88a3024482490dc1a99756
idkzw <zw.io.wayne@gmail.com>
2025-11-05 22:44:22 +0530

    Updated DateSelector.tsx

--- src/components/tasks/DateSelector.tsx
+++ src/components/tasks/DateSelector.tsx
@@ -252,7 +252,7 @@ const DateSelector: React.FC<DateSelectorProps> = ({ selectedDate, onSelect, onT
       </PopoverTrigger>
       <PopoverContent
         className="w-[300px] h-[600px] p-0 bg-[#1b1b1b] border border-[#414141] rounded-[12px] overflow-hidden flex flex-col"
-        align="center"
+        align="start"
         side="bottom"
         sideOffset={8}
       >
